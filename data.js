59b6baaf8125a2b8eed49166ba0d963d904362c7
violentidk <161117561+violentidk@users.noreply.github.com>
2024-11-28 09:31:19 +0200

    Update data.js

--- data.js
+++ data.js
@@ -17,23 +17,6 @@ const productData = [
     },
     lastUpdated: "2024-11-28"
   },
-  {
-    name: "Juiced Breeze Battery 0.33L",
-    prices: {
-      Rimi: 1.69,
-      Coop: 1.75,
-      Selver: 1.73
-    },
-    lastUpdated: "2024-11-28"
-  },
-  {
-    name: "Monster Ultra Gold 0.5L",
-    prices: {
-      Rimi: 1.79,
-      Coop: 1.85,
-      Selver: 1.80
-    },
-    lastUpdated: "2024-11-28"
-  }
+
 ];
  
