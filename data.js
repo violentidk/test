9c15121c08fa9e55f132f0570028ae61f03475e1
violentidk <161117561+violentidk@users.noreply.github.com>
2024-11-28 09:19:41 +0200

    Update and rename script.js to data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+const productData = [
+  {
+    name: "Monster Punch 0.5L",
+    prices: {
+      Rimi: 1.79,
+      Coop: 1.89,
+      Selver: 1.85
+    },
+    lastUpdated: "2024-11-28"
+  },
+  {
+    name: "Burn Original 0.33L",
+    prices: {
+      Rimi: 1.15,
+      Coop: 1.20,
+      Selver: 1.18
+    },
+    lastUpdated: "2024-11-28"
+  },
+  {
+    name: "Juiced Breeze Battery 0.33L",
+    prices: {
+      Rimi: 1.69,
+      Coop: 1.75,
+      Selver: 1.73
+    },
+    lastUpdated: "2024-11-28"
+  },
+  {
+    name: "Monster Ultra Gold 0.5L",
+    prices: {
+      Rimi: 1.79,
+      Coop: 1.85,
+      Selver: 1.80
+    },
+    lastUpdated: "2024-11-28"
+  }
+];
